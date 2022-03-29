d0defe59ab414499fabf538176b3825778603da0
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2022-03-29 15:13:30 -0400

    1.00

--- pages/views.py
+++ pages/views.py
@@ -6,5 +6,14 @@ def home_view(request,*args,**kwargs):
     print(request.user) #prints the user 
     return render(request,"home.html",{})
 
-def contact_view(*args,**kwargs):
-    return HttpResponse("<h1> Hello World</h1>") #String of html code (not actually html)
+def contact_view(request,*args,**kwargs):
+    return render(request,"contact.html",{}) #String of html code (not actually html)
+
+def about_view(request,*args,**kwargs):
+    my_context = { 
+        "my_text" : "this is about me",
+        "my_number" : 123,
+        "my_list" : [123,4242,12313]
+
+    }
+    return render(request,"about.html",my_context)
